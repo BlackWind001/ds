15a7c4f7b7b8273564e713b96ba84391c5ab49ec
BlackWind001 <msanirudh1998@gmail.com>
2024-03-31 03:10:11 +0530

    Made adjustments to the viewport so it will not be responsive and will always consider that the viewport is 1024px

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const viewport = document.querySelector('meta[name="viewport"]');
+const root = document.getElementById('root');
+
+if (viewport && root) {
+  const viewportWidth = root.getBoundingClientRect().width;
+
+  if (viewportWidth < 1024) {
+    viewport.setAttribute('content', 'width=1024, viewport-fit=contain');
+  }
+}
+
+ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
